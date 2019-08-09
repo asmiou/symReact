42c77b6e4bf1b2efd9bd3b11426f860e4ead471f
Hasmiou <17071005+Hasmiou@users.noreply.github.com>
2019-08-09 17:27:00 +0200

    fix process.env.API_URL config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,4 @@
 export const API_URL= process.env.API_URL;
-console.log(API_URL);
 export const INVOICE_URL =`${API_URL}invoices`;
 export const CUSTOMER_URL = `${API_URL}customers`;
 export const USERS_URL =`${API_URL}users`;
