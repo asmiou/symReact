b529f28a7d563f16d5b72436067a3bbcfbe344cb
Hasmiou <17071005+Hasmiou@users.noreply.github.com>
2019-09-09 09:54:40 +0200

    Fixing bug API_URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,5 @@
-export const API_URL= process.env.API_URL;
+//export const API_URL= process.env.API_URL;
+export const API_URL='https://sym-react.herokuapp.com/api/';
 export const INVOICE_URL =`${API_URL}invoices`;
 export const CUSTOMER_URL = `${API_URL}customers`;
 export const USERS_URL =`${API_URL}users`;
